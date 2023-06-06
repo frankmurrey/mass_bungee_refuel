b3123e994fa4439de500ca390a0b22a62d4f8f6a
frankmurrey <sekachgamer@gmail.com>
2023-06-06 16:04:09 +0500

    UPD

--- config.py
+++ config.py
@@ -2,16 +2,16 @@ import contracts
 
 
 # Choose the chains to use, available: "bsc", "polygon", "fantom", "arbitrum", "avalanche
-SOURCE_CHAIN = contracts.polygon
-DESTINATION_CHAIN = contracts.bsc
+SOURCE_CHAIN = contracts.bsc
+DESTINATION_CHAIN = contracts.polygon
 
 # Choose native token amount to refuel, will be randomized between min and max
-MIN_NATIVE_AMOUNT_OUT = 0.01
-MAX_NATIVE_AMOUNT_OUT = 0.02
+MIN_NATIVE_AMOUNT_OUT = 0.0043
+MAX_NATIVE_AMOUNT_OUT = 0.0047
 
 # Choose delay between refuel transactions, will be randomized between min and max
-MIN_DELAY_SECONDS = 10
-MAX_DELAY_SECONDS = 20
+MIN_DELAY_SECONDS = 30
+MAX_DELAY_SECONDS = 40
 
 # Choose max gas limit for refuel transactions, will ebe chosen optimal by 'estimate_gas'
 MAX_GAS_LIMIT = 300000000
